404b47ed91e4b9234f7b5f0096a0fdf4146754fc
Daniel Sørlie Hoftvedt <danisho@stud.ntnu.no>
2024-03-14 18:57:57 +0100

    added feather support, refactored nfc into separate source file

---
+++ nrf52/src/nfc.h
@@ -0,0 +1,10 @@
+#ifndef NFC_H
+#define NFC_H
+
+void print_bytes(uint8_t bytes[], uint32_t len);
+int PN532_wait_for_RDY();
+int pn532_send_receive_message(struct device *dev, uint8_t i2c_address, uint8_t *msg, uint32_t msg_len, uint8_t *res_buf, uint32_t res_buf_len, bool verbose);
+int pn532_nfc_setup();
+int pn532_get_tag_loop();
+
+#endif
