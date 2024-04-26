2ee16c0d662dfd7aa562c52979d92e4e41569001
bjornarvikhagen <vikhagenbjornar@gmail.com>
2024-04-26 09:45:36 +0000

    feat: error handling in forwardservice, try-catch blocks in message sending and mqtt connecting

--- radbak/python-src/fwdservice/fwdservice.py
+++ radbak/python-src/fwdservice/fwdservice.py
@@ -34,41 +34,44 @@ password = "fwdservice"
 
 # Define the callback function for when a message is received
 def on_message(client, userdata, message):
-    # Extract the message payload
-    print("Message recieved!")
-    data = message.payload.decode()
-    print(f"{message.timestamp} | {data}")
+    try:
+        # Extract the message payload
+        print("Message recieved!")
+        data = message.payload.decode()
+        print(f"{message.timestamp} | {data}")
 
-    # Assume messages are recieved like this:
-    # CheckpointID:RFID:Timestamp
-    # Example: 3:Tag1:2024-04-15T11:39:02.561Z  # Tag1 belongs to Erik
+        # Assume messages are recieved like this:
+        # CheckpointID:RFID:Timestamp
+        # Example: 3:Tag1:2024-04-15T11:39:02.561Z  # Tag1 belongs to Erik
 
-    # Format the message data
-    split_data = data.split(":")
+        # Format the message data
+        split_data = data.split(":")
 
-    device_id = split_data[0]
-    rfid = split_data[1]
-    timestamp = datetime.now() - timedelta(milliseconds=int(split_data[2]))
+        device_id = split_data[0]
+        rfid = split_data[1]
+        timestamp = datetime.now() - timedelta(milliseconds=int(split_data[2]))
 
-    api_payload = {
-        "TagID": rfid,
-        "DeviceID": device_id,
-        "PassingTime": str(timestamp),
-    }
-    print("Sending pyload: ", str(api_payload))
+        api_payload = {
+            "TagID": rfid,
+            "DeviceID": device_id,
+            "PassingTime": str(timestamp),
+        }
+        print("Sending pyload: ", str(api_payload))
 
-    # Make a POST request to the API with the message data
-    response = requests.post(
-        # api_endpoint, data=json.dumps(api_payload)
-        api_endpoint,
-        data=json.dumps(api_payload),
-    )
+        # Make a POST request to the API with the message data
+        response = requests.post(
+            # api_endpoint, data=json.dumps(api_payload)
+            api_endpoint,
+            data=json.dumps(api_payload),
+        )
 
-    if response.status_code == 200:
-        print("Data successfully forwarded to API!")
-    else:
-        print("Failed to send data to API...")
-        print(response.text)
+        if response.status_code == 200:
+            print("Data successfully forwarded to API!")
+        else:
+            print("Failed to send data to API...")
+            print(response.text)
+    except Exception as e:
+        print(f"Failed to send data to API, error processing message: {e}")
 
 
 # Callback when the client receives a CONNACK response from the server. I.e. This is called once we have a connection.
@@ -89,33 +92,37 @@ def on_subscribe(client, userdata, mid, reason_code_list, properties):
 
 
 def connect_to_mqtt():
-    # Create a new MQTT client instance
-    client = mqtt.Client(
-        callback_api_version=mqtt_enums.CallbackAPIVersion.VERSION2, client_id=client_id
-    )
-    client.username_pw_set(username, password)
-    client.tls_set(ca_certs="./fwdservice/emqxsl-ca.crt")  # Set certificate for TLS
+    try:
+        # Create a new MQTT client instance
+        client = mqtt.Client(
+            callback_api_version=mqtt_enums.CallbackAPIVersion.VERSION2,
+            client_id=client_id,
+        )
+        client.username_pw_set(username, password)
+        client.tls_set(ca_certs="./fwdservice/emqxsl-ca.crt")  # Set certificate for TLS
 
-    # Assign the on_connect callback function
-    client.on_connect = on_connect
+        # Assign the on_connect callback function
+        client.on_connect = on_connect
 
-    # Set the on_message function
-    client.on_message = on_message
+        # Set the on_message function
+        client.on_message = on_message
 
-    # Confirm subscription status
-    client.on_subscribe = on_subscribe
+        # Confirm subscription status
+        client.on_subscribe = on_subscribe
 
-    # Connect to the broker
-    print(f"Connecting to {broker_address}")
-    client.connect(
-        broker_address, broker_port, keepalive=10 * 60 * 60
-    )  # Keep alive for 10 hours
+        # Connect to the broker
+        print(f"Connecting to {broker_address}")
+        client.connect(
+            broker_address, broker_port, keepalive=10 * 60 * 60
+        )  # Keep alive for 10 hours
 
-    # Publishing a hello-message to a topic
-    client.publish(topic, "Hello, EMQX!")
+        # Publishing a hello-message to a topic
+        client.publish(topic, "Hello, EMQX!")
 
-    # Blocking call that processes network traffic, dispatches callbacks, and handles reconnecting.
-    client.loop_forever(timeout=3600 * 10)  # 10 hour timeout for now
+        # Blocking call that processes network traffic, dispatches callbacks, and handles reconnecting.
+        client.loop_forever(timeout=3600 * 10)  # 10 hour timeout for now
+    except Exception as e:
+        print(f"Failed to connect or error during mqtt opera: {e}")
 
 
 if __name__ == "__main__":
